9673383f51fb28a8902740718676539666f0ab34
Anubhav Bisht <40894333+anubhavbisht@users.noreply.github.com>
2021-05-07 20:39:28 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -5,23 +5,30 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      message: "",
+      message: false,
     };
   }
   change() {
-    console.log("hello");
-    this.setState({
-      message:
-        "Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy",
-    });
+    this.setState({ message: true });
   }
+
   render() {
+    const isLoggedIn = this.state.message;
     return (
       <div id="main">
-        <button id="click" onClick={() => this.change()}>
-          Click
-        </button>
-        <p id="para">{this.state.message}</p>
+        <div>
+          <button id="click" onClick={() => this.change()}>
+            Click
+          </button>
+        </div>
+        <div>
+          {isLoggedIn ? (
+            <p id="para">
+              Hello, I've learnt to use the full-stack evaluation tool. This
+              makes me so happy
+            </p>
+          ) : null}
+        </div>
       </div>
     );
   }
